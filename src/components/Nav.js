9465dfb3818e882e772222b0a08dfa1fc83f99e8
Yasir Haleem <yasirhaleem@gmail.com>
2020-06-21 22:20:54 +0500

    initial commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -23,7 +23,7 @@ function Nav(props){
             <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
                 <div className="text-sm lg:flex-grow">
                 <Link to="/" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-                    Home
+                    Home 
                 </Link>
                 <Link to="/about" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
                     About
